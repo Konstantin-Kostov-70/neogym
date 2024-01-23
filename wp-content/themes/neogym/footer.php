c4b9800a911f3c3704b07c5fb31f8491b70f02f6
kosta.norge@gmail.com <kosta.norge@gmail.com>
2024-01-23 21:24:05 +0100

    delete href in a tag in footer

--- wp-content/themes/neogym/footer.php
+++ wp-content/themes/neogym/footer.php
@@ -2,7 +2,7 @@
  <footer class="container-fluid footer_section">
     <p>
       &copy; 2020 All Rights Reserved. Design by
-      <a href="https://html.design/">Free Html Templates</a>
+      <a href="">Free Html Templates</a>
     </p>
   </footer>
   <!-- footer section -->
