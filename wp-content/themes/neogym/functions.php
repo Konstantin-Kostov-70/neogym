537370cd21944092298f5fd154fb7d1a9e18a034
kosta.norge@gmail.com <kosta.norge@gmail.com>
2024-01-23 21:22:40 +0100

    update comments to neogym_assets func

--- wp-content/themes/neogym/functions.php
+++ wp-content/themes/neogym/functions.php
@@ -8,7 +8,7 @@ function neogym_assets() {
    wp_enqueue_style('owl-carousel', '//cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css', false, '1.0.0');
    wp_enqueue_style('google-fonts', '//fonts.googleapis.com/css?family=Poppins:400,600,700&display=swap', false, '1.0.0');
    
-   // This scripts is loaded in footer.php with function wp_footer()
+   // This scripts is loaded in footer.php with function wp_footer() , the last argument is set to true
    wp_enqueue_script('bootstrap-js', get_theme_file_uri('/assets/js/bootstrap.js'), array(), '1.0.0', true );
    wp_enqueue_script('bootstrap-js', get_theme_file_uri('/assets/js/jquery-3.4.1.min.js'), array('jquery'), '1.0.0', true );
 }
