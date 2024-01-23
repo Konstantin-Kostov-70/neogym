4f2799cb0553d905e08f10eee9cf7bf12c955c05
kosta.norge@gmail.com <kosta.norge@gmail.com>
2024-01-23 04:50:17 +0100

    delete all link because we use wp_head function

--- wp-content/themes/neogym/header.php
+++ wp-content/themes/neogym/header.php
@@ -16,17 +16,15 @@
   <title><?php the_title(); ?></title>
 
   <!-- slider stylesheet -->
-  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
 
   <!-- bootstrap core css -->
-  <link rel="stylesheet" type="text/css" href="css/bootstrap.css" />
 
   <!-- fonts style -->
-  <link href="https://fonts.googleapis.com/css?family=Poppins:400,600,700&display=swap" rel="stylesheet">
+  
   <!-- Custom styles for this template -->
-  <link href="css/style.css" rel="stylesheet" />
+
   <!-- responsive style -->
-  <link href="css/responsive.css" rel="stylesheet" />
+ 
 </head>
 
 <body>
