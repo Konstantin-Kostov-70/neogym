e80e526ec516c671626c0e6499b1d62a2d965c15
kosta.norge@gmail.com <kosta.norge@gmail.com>
2024-01-23 02:31:31 +0100

    create footer.php header.php files

---
+++ wp-content/themes/neogym/footer.php
@@ -0,0 +1,15 @@
+ <!-- footer section -->
+ <footer class="container-fluid footer_section">
+    <p>
+      &copy; 2020 All Rights Reserved. Design by
+      <a href="https://html.design/">Free Html Templates</a>
+    </p>
+  </footer>
+  <!-- footer section -->
+
+  <script src="js/jquery-3.4.1.min.js"></script>
+  <script src="js/bootstrap.js"></script>
+<?php wp_footer(); ?>
+</body>
+
+</html>
