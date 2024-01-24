9b3023e3a15d7f049a54c0fe5a7a7a005dfc633c
kosta.norge@gmail.com <kosta.norge@gmail.com>
2024-01-24 06:37:29 +0100

    create blog.php file in templates

---
+++ wp-content/themes/neogym/templates/blog.php
@@ -0,0 +1,165 @@
+<?php /* Template Name: Blog */ ?> 
+<?php get_header(); ?>
+
+
+  <!-- Us section -->
+
+  <section class="us_section layout_padding">
+    <div class="container">
+      <div class="heading_container">
+        <h2>
+          Our Posts
+        </h2>
+      </div>
+
+      <div class="us_container ">
+        <div class="row">
+          <div class="col-lg-3 col-md-6">
+            <div class="box">
+              <div class="img-box">
+                <img src="images/u-1.png" alt="">
+              </div>
+              <div class="detail-box">
+                <h5>
+                  QUALITY EQUIPMENT
+                </h5>
+                <p>
+                  ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6">
+            <div class="box">
+              <div class="img-box">
+                <img src="images/u-4.png" alt="">
+              </div>
+              <div class="detail-box">
+                <h5>
+                  NUTRITION
+                </h5>
+                <p>
+                  ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6">
+            <div class="box">
+              <div class="img-box">
+                <img src="images/u-2.png" alt="">
+              </div>
+              <div class="detail-box">
+                <h5>
+                  HEALTHY DIET PLAN
+                </h5>
+                <p>
+                  ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6">
+            <div class="box">
+              <div class="img-box">
+                <img src="images/u-3.png" alt="">
+              </div>
+              <div class="detail-box">
+                <h5>
+                  SPORT TRAINING
+                </h5>
+                <p>
+                  ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- end us section -->
+
+
+  <!-- heathy section -->
+
+  <section class="heathy_section layout_padding">
+    <div class="container">
+
+      <div class="row">
+        <div class="col-md-12 mx-auto">
+          <div class="detail-box">
+            <h2>
+              HEALTHY MIND, HEALTHY BODY
+            </h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillumLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+            </p>
+            <div class="btn-box">
+              <a href="">
+                READ MORE
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </section>
+
+  <!-- end heathy section -->
+
+ 
+
+  <!-- contact section -->
+
+
+  <!-- end contact section -->
+
+  <!-- info section -->
+  <section class="info_section layout_padding2">
+    <div class="container">
+      <div class="info_items">
+        <a href="">
+          <div class="item ">
+            <div class="img-box box-1">
+              <img src="" alt="">
+            </div>
+            <div class="detail-box">
+              <p>
+                Location
+              </p>
+            </div>
+          </div>
+        </a>
+        <a href="">
+          <div class="item ">
+            <div class="img-box box-2">
+              <img src="" alt="">
+            </div>
+            <div class="detail-box">
+              <p>
+                +02 1234567890
+              </p>
+            </div>
+          </div>
+        </a>
+        <a href="">
+          <div class="item ">
+            <div class="img-box box-3">
+              <img src="" alt="">
+            </div>
+            <div class="detail-box">
+              <p>
+                demo@gmail.com
+              </p>
+            </div>
+          </div>
+        </a>
+      </div>
+    </div>
+  </section>
+
+  <!-- end info_section -->
+<?php get_footer(); ?>
