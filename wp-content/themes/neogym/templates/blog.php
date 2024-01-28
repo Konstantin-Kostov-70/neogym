38c46ea089c88a2c8fc1ab9036bb15cd3f577940
kosta.norge@gmail.com <kosta.norge@gmail.com>
2024-01-28 19:50:56 +0100

    use func list_all_posts in template

--- wp-content/themes/neogym/templates/blog.php
+++ wp-content/themes/neogym/templates/blog.php
@@ -14,7 +14,8 @@
 
       <div class="us_container ">
         <div class="row">
-          <div class="col-lg-3 col-md-6">
+          <?php list_all_posts(4) ?>
+          <!-- <div class="col-lg-3 col-md-6">
             <div class="box">
               <div class="img-box">
                 <img src="images/u-1.png" alt="">
@@ -73,7 +74,7 @@
                 </p>
               </div>
             </div>
-          </div>
+          </div> -->
         </div>
       </div>
     </div>
@@ -117,49 +118,5 @@
 
   <!-- end contact section -->
 
-  <!-- info section -->
-  <section class="info_section layout_padding2">
-    <div class="container">
-      <div class="info_items">
-        <a href="">
-          <div class="item ">
-            <div class="img-box box-1">
-              <img src="" alt="">
-            </div>
-            <div class="detail-box">
-              <p>
-                Location
-              </p>
-            </div>
-          </div>
-        </a>
-        <a href="">
-          <div class="item ">
-            <div class="img-box box-2">
-              <img src="" alt="">
-            </div>
-            <div class="detail-box">
-              <p>
-                +02 1234567890
-              </p>
-            </div>
-          </div>
-        </a>
-        <a href="">
-          <div class="item ">
-            <div class="img-box box-3">
-              <img src="" alt="">
-            </div>
-            <div class="detail-box">
-              <p>
-                demo@gmail.com
-              </p>
-            </div>
-          </div>
-        </a>
-      </div>
-    </div>
-  </section>
-
-  <!-- end info_section -->
+ 
 <?php get_footer(); ?>
