8e5851388f7570428a96ebf8a3ec06422cf545ba
kosta.norge@gmail.com <kosta.norge@gmail.com>
2024-01-28 19:42:31 +0100

    add main file to custom plugin

---
+++ wp-content/plugins/neogym-plugin/neogym.php
@@ -0,0 +1,16 @@
+<?php
+ /*
+ * Plugin Name:       Neogym Plugin
+ * Plugin URI:        https://softuni.bg
+ * Description:       Plugin that adds more custom functionality
+ * Version:           1.0.0
+ * Requires at least: 5.0
+ * Requires PHP:      8.0
+ * Author:            Konstantin Kostov
+ * Author URI:        https://softuni.bg/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://example.com/my-plugin/
+ * Text Domain:       softuni
+ * Domain Path:       /languages
+ */
