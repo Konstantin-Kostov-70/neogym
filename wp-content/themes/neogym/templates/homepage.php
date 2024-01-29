53c71fdce66755f22d6f6263840b8ae3566a4395
kosta.norge@gmail.com <kosta.norge@gmail.com>
2024-01-29 09:26:42 +0100

    add func list_all_traners

--- wp-content/themes/neogym/templates/homepage.php
+++ wp-content/themes/neogym/templates/homepage.php
@@ -267,79 +267,13 @@
     <div class="container">
       <div class="heading_container">
         <h2>
-          Our Gym Trainers
+          <a href="<?php echo site_url('/our-trainers') ?>">
+            Our Gym Trainers
+          </a>
         </h2>
       </div>
       <div class="row">
-        <div class="col-lg-4 col-md-6 mx-auto">
-          <div class="box">
-            <div class="name">
-              <h5>
-                Smirth Jon
-              </h5>
-            </div>
-            <div class="img-box">
-              <img src="<?php echo get_theme_file_uri('/assets/images/t1.jpg') ?>" alt="">
-            </div>
-            <div class="social_box">
-              <a href="">
-                <img src="<?php echo get_theme_file_uri('/assets/images/facebook-logo.png') ?>" alt="">
-              </a>
-              <a href="">
-                <img src="images/twitter.png" alt="">
-              </a>
-              <a href="">
-                <img src="images/instagram-logo.png" alt="">
-              </a>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-4 col-md-6 mx-auto">
-          <div class="box">
-            <div class="name">
-              <h5>
-                Jean Doe
-              </h5>
-            </div>
-            <div class="img-box">
-              <img src="images/t2.jpg" alt="">
-            </div>
-            <div class="social_box">
-              <a href="">
-                <img src="images/facebook-logo.png" alt="">
-              </a>
-              <a href="">
-                <img src="images/twitter.png" alt="">
-              </a>
-              <a href="">
-                <img src="images/instagram-logo.png" alt="">
-              </a>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-4 col-md-6 mx-auto">
-          <div class="box">
-            <div class="name">
-              <h5>
-                Alex Den
-              </h5>
-            </div>
-            <div class="img-box">
-              <img src="images/t3.jpg" alt="">
-            </div>
-            <div class="social_box">
-              <a href="">
-                <img src="images/facebook-logo.png" alt="">
-              </a>
-              <a href="">
-                <img src="images/twitter.png" alt="">
-              </a>
-              <a href="">
-                <img src="images/instagram-logo.png" alt="">
-              </a>
-            </div>
-          </div>
-        </div>
+        <?php list_all_trainers(3); ?>
       </div>
     </div>
   </section>
