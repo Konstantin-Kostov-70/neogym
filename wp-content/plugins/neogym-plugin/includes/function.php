b5a577d8a2027ac4c2a6529dd70781477fcac5eb
kosta.norge@gmail.com <kosta.norge@gmail.com>
2024-01-29 09:31:09 +0100

    formatting source code

--- wp-content/plugins/neogym-plugin/includes/function.php
+++ wp-content/plugins/neogym-plugin/includes/function.php
@@ -1,17 +1,17 @@
 <?php
 //Here is generic functions of custom plugin
 
-add_theme_support('post-thumbnails');
+add_theme_support( 'post-thumbnails' );
 
 /**
  * This function list all post from custom post type 'trainer'
  */
-function list_all_trainers($post_per_page) {
+function list_all_trainers( $post_per_page ) {
  
      $trainer_query_args = array(
-        'post_type'     => 'trainer',
-        'post_status'   => 'publish',
-        'post_per_page' => $post_per_page,
+        'post_type'      => 'trainer',
+        'post_status'    => 'publish',
+        'post_per_page'  => $post_per_page,
         'paged'          => get_query_var('paged'),
        
      );
@@ -19,9 +19,9 @@ function list_all_trainers($post_per_page) {
     $trainer_query = new WP_Query( $trainer_query_args );
  
     ?>
-    <?php if($trainer_query->have_posts()) : ?>
-       <?php while($trainer_query->have_posts()) : $trainer_query->the_post(); ?>
-         <?php get_template_part('partials/content', 'trainer') ?>
+    <?php if( $trainer_query->have_posts() ) : ?>
+       <?php while( $trainer_query->have_posts() ) : $trainer_query->the_post(); ?>
+         <?php get_template_part( 'partials/content', 'trainer' ) ?>
        <?php endwhile; ?>
     <?php endif; ?>
     <?php
