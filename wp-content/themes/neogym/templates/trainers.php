d9b9e68032e88498b97c7753b23f9f216a7362d3
kosta.norge@gmail.com <kosta.norge@gmail.com>
2024-01-29 09:27:36 +0100

    create template trainers.php

---
+++ wp-content/themes/neogym/templates/trainers.php
@@ -0,0 +1,44 @@
+<?php /* Template Name: Trainers */ ?> 
+<?php get_header(); ?>
+
+
+<section class="trainer_section layout_padding">
+    <div class="container">
+      <div class="heading_container">
+        <h2>
+            Our Gym Trainers
+        </h2>
+      </div>
+      <div class="row">
+        <?php list_all_trainers(9); ?>
+      </div>
+    </div>
+  </section>
+
+
+  <!-- heathy section -->
+
+  <section class="heathy_section layout_padding">
+    <div class="container">
+
+      <div class="row">
+        <div class="col-md-12 mx-auto">
+          <div class="detail-box">
+            <h2>
+              HEALTHY MIND, HEALTHY BODY
+            </h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillumLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
+            </p>
+            <div class="btn-box">
+              <a href="">
+                READ MORE
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </section>
+<?php get_footer(); ?>
