e04a39e0e3030cd3d9eccc8d9c66b07a14d4d2b4
kosta.norge@gmail.com <kosta.norge@gmail.com>
2024-01-29 09:22:57 +0100

    create template parts

---
+++ wp-content/themes/neogym/partials/content-trainer.php
@@ -0,0 +1,26 @@
+
+<div class="col-lg-4 col-md-6 mx-auto">
+          <div class="box">
+            <div class="name">
+              <h5>
+                <?php the_title(); ?>
+              </h5>
+            </div>
+            <div class="img-box">
+                <a href="<?php the_permalink(); ?>">
+                    <?php the_post_thumbnail('post-thumbnail'); ?>
+                </a>
+            </div>
+            <div class="social_box">
+              <a href="">
+                <img src="<?php echo get_theme_file_uri('/assets/images/facebook-logo.png') ?>" alt="">
+              </a>
+              <a href="">
+                <img src="<?php echo get_theme_file_uri('/assets/images/twitter.png') ?>" alt="">
+              </a>
+              <a href="">
+                <img src="<?php echo get_theme_file_uri('/assets/images/instagram-logo.png') ?>" alt="">
+              </a>
+            </div>
+          </div>
+        </div>
