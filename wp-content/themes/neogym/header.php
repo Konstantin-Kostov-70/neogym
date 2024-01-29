56c69d8f1151bc8d9b3db122150f879ec47187cc
kosta.norge@gmail.com <kosta.norge@gmail.com>
2024-01-29 09:19:34 +0100

    formatting source code

--- wp-content/themes/neogym/header.php
+++ wp-content/themes/neogym/header.php
@@ -46,10 +46,10 @@
             <!-- <div class="d-flex ml-auto flex-column flex-lg-row align-items-center">
               <ul class="navbar-nav  ">
                 <li class="nav-item active">
-                  <a class="nav-link" href="<?php get_site_url('/') ?>">Home <span class="sr-only">(current)</span></a>
+                  <a class="nav-link" href="">Home <span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item ">
-                  <a class="nav-link" href="<?php get_site_url('/our-blog') ?>"> Blog </a>
+                  <a class="nav-link" href=""> Blog </a>
                 </li>
                 </li>
                 <li class="nav-item">
