11e3303d8281f959d323e9c5771463fe597690a5
kosta.norge@gmail.com <kosta.norge@gmail.com>
2024-02-01 21:59:57 +0100

    Create single for program CPT

---
+++ wp-content/themes/neogym/single-program.php
@@ -0,0 +1,37 @@
+<?php get_header(); ?>
+
+<!-- heathy section -->
+<?php if (have_posts()) : ?>
+  <?php while (have_posts()) : the_post(); ?>
+    <section class="heathy_section layout_padding">
+      <div class="container">
+
+        <div class="row">
+          <div class="col-md-12 mx-auto">
+            <div class="detail-box">
+              <h2>
+                <?php the_title(); ?>
+              </h2>
+              <p>
+                <?php the_content(); ?>
+              </p>
+
+              <div class="btn-box">
+                <a class="program_joined" href="" data-post-id="<?php echo get_the_ID(); ?>">
+                  JOIN TO PROGRAM
+                </a>
+                <div class="joined-wrapper">
+                  <span>JOINED PEOPLE</span>
+                  <span class="joined-span"><?php echo get_post_meta(get_the_ID(), 'joined', true) ?></span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section>
+  <?php endwhile; ?>
+<?php endif; ?>
+
+<?php get_footer(); ?>
