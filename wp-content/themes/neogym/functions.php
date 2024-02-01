11844a5af3afc94e5929a5600ba2a605f238bfc4
kosta.norge@gmail.com <kosta.norge@gmail.com>
2024-02-01 21:57:39 +0100

    formatting source code

--- wp-content/themes/neogym/functions.php
+++ wp-content/themes/neogym/functions.php
@@ -1,5 +1,5 @@
 <?php
-// Here is all functions for Neogym theme
+// Here is generic functions for Neogym theme
 
 add_theme_support('post-thumbnails');
 /**
@@ -7,18 +7,18 @@ add_theme_support('post-thumbnails');
  */
 function neogym_assets()
 {
-  wp_enqueue_style('style-css', get_theme_file_uri('/assets/css/style.css'), false, '1.0.0');
-  wp_enqueue_style('responsive-css', get_theme_file_uri('/assets/css/responsive.css'), false, '1.0.0');
-  wp_enqueue_style('bootstrap-css', get_theme_file_uri('/assets/css/bootstrap.css'), false, '1.0.0');
-  wp_enqueue_style('owl-carousel', '//cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css', false, '1.0.0');
-  wp_enqueue_style('google-fonts', '//fonts.googleapis.com/css?family=Poppins:400,600,700&display=swap', false, '1.0.0');
+  wp_enqueue_style( 'style-css', get_theme_file_uri( '/assets/css/style.css' ), false, '1.0.0' );
+  wp_enqueue_style( 'responsive-css', get_theme_file_uri( '/assets/css/responsive.css' ), false, '1.0.0' );
+  wp_enqueue_style( 'bootstrap-css', get_theme_file_uri( '/assets/css/bootstrap.css' ), false, '1.0.0' );
+  wp_enqueue_style( 'owl-carousel', '//cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css', false, '1.0.0' );
+  wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Poppins:400,600,700&display=swap', false, '1.0.0' );
 
   // This scripts is loaded in footer.php with function wp_footer() , the last argument is set to true because we wont this functionality
-  wp_enqueue_script('bootstrap-js', get_theme_file_uri('/assets/js/bootstrap.js'), array(), '1.0.0', true);
-  wp_enqueue_script('bootstrap-js', get_theme_file_uri('/assets/js/jquery-3.4.1.min.js'), array('jquery'), '1.0.0', true);
+  wp_enqueue_script( 'bootstrap-js', get_theme_file_uri( '/assets/js/bootstrap.js' ), array(), '1.0.0', true );
+  wp_enqueue_script( 'bootstrap-js', get_theme_file_uri( '/assets/js/jquery-3.4.1.min.js' ), array('jquery'), '1.0.0', true );
 }
 
-add_action('wp_enqueue_scripts', 'neogym_assets');
+add_action( 'wp_enqueue_scripts', 'neogym_assets' );
 
 /**
  * This function register menus for us
@@ -26,11 +26,54 @@ add_action('wp_enqueue_scripts', 'neogym_assets');
 function neogym_register_menu()
 {
   register_nav_menus(array(
-    'primary_menu' => __('Primary Menu', 'softuni')
+    'primary_menu' => __( 'Primary Menu', 'softuni' ),
+    'location'     => __( 'Location', 'softuni' ),
+    'phone'        => __( 'Phone', 'softuni' ),
+    'email'        => __( 'Email', 'softuni' ),
   ));
 }
 
-add_action('after_setup_theme', 'neogym_register_menu');
+add_action( 'after_setup_theme', 'neogym_register_menu' );
+
+
+/**
+ * This function register sidebars
+ */
+function neogym_register_sidebar()
+{
+	register_sidebar(array(
+		'id'            => 'footer-1',
+		'name'          => __('Footer 01'),
+		'description'   => __('Widgets in this area will be shown on all posts and pages.', 'textdomain'),
+		'before_widget' => '<li id="%1$s" class="widget %2$s" style="list-style: none">',
+		'after_widget'  => '</li>',
+		'before_title'  => '<h2 class="widgettitle">',
+		'after_title'   => '</h2>',
+	));
+
+	register_sidebar(array(
+		'id'            => 'footer-2',
+		'name'          => __('Footer 02'),
+		'description'   => __('Widgets in this area will be shown on all posts and pages.', 'textdomain'),
+		'before_widget' => '<li id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</li>',
+		'before_title'  => '<h2 class="widgettitle">',
+		'after_title'   => '</h2>',
+	));
+
+	register_sidebar(array(
+		'id'            => 'footer-3',
+		'name'          => __('Footer 03'),
+		'description'   => __('Widgets in this area will be shown on all posts and pages.', 'textdomain'),
+		'before_widget' => '<li id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</li>',
+		'before_title'  => '<h2 class="widgettitle">',
+		'after_title'   => '</h2>',
+	));
+}
+
+add_action( 'widgets_init', 'neogym_register_sidebar' );
+
 
 
 // function add_menu_list_item_class($classes, $item, $args) {
