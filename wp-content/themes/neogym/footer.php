8a43ff7ee6fd448b923d8c91b9775d4051beb849
kosta.norge@gmail.com <kosta.norge@gmail.com>
2024-02-01 21:56:50 +0100

    add sidebar in footer

--- wp-content/themes/neogym/footer.php
+++ wp-content/themes/neogym/footer.php
@@ -1,70 +1,75 @@
  <!-- info section -->
  <section class="info_section layout_padding2">
-    <div class="container">
-      <div class="info_items">
-        <a href="">
-          <div class="item ">
-            <div class="img-box box-1">
-              <img src="" alt="">
-            </div>
-            <div class="detail-box">
-              <p>
-                Location
-              </p>
-            </div>
-          </div>
-        </a>
-        <a href="">
-          <div class="item ">
-            <div class="img-box box-2">
-              <img src="" alt="">
-            </div>
-            <div class="detail-box">
-              <p>
-                +02 1234567890
-              </p>
-            </div>
-          </div>
-        </a>
-        <a href="">
-          <div class="item ">
-            <div class="img-box box-3">
-              <img src="" alt="">
-            </div>
-            <div class="detail-box">
-              <p>
-                demo@gmail.com
-              </p>
-            </div>
-          </div>
-        </a>
-      </div>
-    </div>
-  </section>
+   <div class="container">
+     <div class="info_items">
 
-  <!-- end info_section -->
- 
- 
+       <div class="item ">
+         <div class="img-box box-1">
+           <img src="" alt="">
+         </div>
+         <div class="detail-box">
+           <p>
+             Location
+           </p>
+           <?php
+           dynamic_sidebar('footer-1')
+            ?>
+         </div>
+       </div>
+
+
+       <div class="item ">
+         <div class="img-box box-2">
+           <img src="" alt="">
+         </div>
+         <div class="detail-box">
+           <p>
+             +02 1234567890
+           </p>
+           <?php
+           dynamic_sidebar('footer-2')
+            ?>
+         </div>
+       </div>
+
+
+       <div class="item ">
+         <div class="img-box box-3">
+           <img src="" alt="">
+         </div>
+         <div class="detail-box">
+           <p>
+             demo@gmail.com
+           </p>
+           <?php
+           dynamic_sidebar('footer-3')
+            ?>
+         </div>
+       </div>
+     </div>
+   </div>
+ </section>
+
+ <!-- end info_section -->
+
+
  <!-- footer section -->
  <footer class="container-fluid footer_section">
-    <p>
-      &copy; 2020 All Rights Reserved. Design by
-      <span>Free Html Templates</span>
-    </p>
-  </footer>
-  <!-- footer section -->
+   <p>
+     &copy; 2020 All Rights Reserved. Design by
+     <span>Free Html Templates</span>
+   </p>
+ </footer>
+ <!-- footer section -->
 
-  <script src="js/jquery-3.4.1.min.js"></script>
-  <script src="js/bootstrap.js"></script>
-  
-<?php
-/**
-	 * Prints scripts or data before the closing body tag on the front end.
-	 *
-	 * @since 1.5.1
-	 */
- wp_footer(); 
- ?>
-</body>
+ <?php
+  /**
+   * Prints scripts or data before the closing body tag on the front end.
+   *
+   * @since 1.5.1
+   */
+  wp_footer();
+  ?>
+ </body>
 
-</html>
+ </html>
