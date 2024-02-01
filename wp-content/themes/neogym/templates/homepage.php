414238b0d4386a8121954d3bff1777344816743c
kosta.norge@gmail.com <kosta.norge@gmail.com>
2024-02-01 22:04:40 +0100

    add func list_all_programs in homepage

--- wp-content/themes/neogym/templates/homepage.php
+++ wp-content/themes/neogym/templates/homepage.php
@@ -1,289 +1,273 @@
-<?php /* Template Name: Homepage */ ?> 
+<?php /* Template Name: Homepage */ ?>
 <?php get_header(); ?>
-        <!-- slider section -->
-        <section class=" slider_section position-relative">
-      <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <div class="container">
-              <div class="col-lg-10 col-md-11 mx-auto">
-                <div class="detail-box">
-                  <div>
-                    <h3>
-                      Fitness
-                    </h3>
-                    <h2>
-                      Training
-                    </h2>
-                    <h1>
-                      Neogym
-                    </h1>
-                    <p>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse .
-                    </p>
-                    <div class="">
-                      <a href="">
-                        Contact Us
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="carousel-item">
-            <div class="container">
-              <div class="col-lg-10 col-md-11 mx-auto">
-                <div class="detail-box">
-                  <div>
-                    <h3>
-                      Fitness
-                    </h3>
-                    <h2>
-                      Training
-                    </h2>
-                    <h1>
-                      Neogym
-                    </h1>
-                    <p>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse .
-                    </p>
-                    <div class="">
-                      <a href="">
-                        Contact Us
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="carousel-item">
-            <div class="container">
-              <div class="col-lg-10 col-md-11 mx-auto">
-                <div class="detail-box">
-                  <div>
-                    <h3>
-                      Fitness
-                    </h3>
-                    <h2>
-                      Training
-                    </h2>
-                    <h1>
-                      Neogym
-                    </h1>
-                    <p>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse .
-                    </p>
-                    <div class="">
-                      <a href="">
-                        Contact Us
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="carousel-item">
-            <div class="container">
-              <div class="col-lg-10 col-md-11 mx-auto">
-                <div class="detail-box">
-                  <div>
-                    <h3>
-                      Fitness
-                    </h3>
-                    <h2>
-                      Training
-                    </h2>
-                    <h1>
-                      Neogym
-                    </h1>
-                    <p>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse .
-                    </p>
-                    <div class="">
-                      <a href="">
-                        Contact Us
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="carousel-item">
-            <div class="container">
-              <div class="col-lg-10 col-md-11 mx-auto">
-                <div class="detail-box">
-                  <div>
-                    <h3>
-                      Fitness
-                    </h3>
-                    <h2>
-                      Training
-                    </h2>
-                    <h1>
-                      Neogym
-                    </h1>
-                    <p>
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse .
-                    </p>
-                    <div class="">
-                      <a href="">
-                        Contact Us
-                      </a>
-                    </div>
-                  </div>
+<!-- slider section -->
+<section class=" slider_section position-relative">
+  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <div class="container">
+          <div class="col-lg-10 col-md-11 mx-auto">
+            <div class="detail-box">
+              <div>
+                <h3>
+                  Fitness
+                </h3>
+                <h2>
+                  Training
+                </h2>
+                <h1>
+                  Neogym
+                </h1>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse .
+                </p>
+                <div class="">
+                  <a href="">
+                    Contact Us
+                  </a>
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <ol class="carousel-indicators">
-          <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-          <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-          <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-          <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
-          <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
-        </ol>
       </div>
-    </section>
-    <!-- end slider section -->
+      <div class="carousel-item">
+        <div class="container">
+          <div class="col-lg-10 col-md-11 mx-auto">
+            <div class="detail-box">
+              <div>
+                <h3>
+                  Fitness
+                </h3>
+                <h2>
+                  Training
+                </h2>
+                <h1>
+                  Neogym
+                </h1>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse .
+                </p>
+                <div class="">
+                  <a href="">
+                    Contact Us
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <div class="container">
+          <div class="col-lg-10 col-md-11 mx-auto">
+            <div class="detail-box">
+              <div>
+                <h3>
+                  Fitness
+                </h3>
+                <h2>
+                  Training
+                </h2>
+                <h1>
+                  Neogym
+                </h1>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse .
+                </p>
+                <div class="">
+                  <a href="">
+                    Contact Us
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <div class="container">
+          <div class="col-lg-10 col-md-11 mx-auto">
+            <div class="detail-box">
+              <div>
+                <h3>
+                  Fitness
+                </h3>
+                <h2>
+                  Training
+                </h2>
+                <h1>
+                  Neogym
+                </h1>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse .
+                </p>
+                <div class="">
+                  <a href="">
+                    Contact Us
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="carousel-item">
+        <div class="container">
+          <div class="col-lg-10 col-md-11 mx-auto">
+            <div class="detail-box">
+              <div>
+                <h3>
+                  Fitness
+                </h3>
+                <h2>
+                  Training
+                </h2>
+                <h1>
+                  Neogym
+                </h1>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse .
+                </p>
+                <div class="">
+                  <a href="">
+                    Contact Us
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <ol class="carousel-indicators">
+      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
+      <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
+    </ol>
   </div>
+</section>
+<!-- end slider section -->
+</div>
 
 
-  <!-- Us section -->
+<!-- Us section -->
 
-  <section class="us_section layout_padding">
-    <div class="container">
-      <div class="heading_container">
-        <h2>
-          Why Choose Us
-        </h2>
-      </div>
+<section class="us_section layout_padding">
+  <div class="container">
+    <div class="heading_container">
+      <h2>
+        Why Choose Us
+      </h2>
+    </div>
 
-      <div class="us_container ">
-        <div class="row">
-          <div class="col-lg-3 col-md-6">
-            <div class="box">
-              <div class="img-box">
-                <img src="images/u-1.png" alt="">
-              </div>
-              <div class="detail-box">
-                <h5>
-                  QUALITY EQUIPMENT
-                </h5>
-                <p>
-                  ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                </p>
-              </div>
+    <div class="us_container ">
+      <div class="row">
+        <div class="col-lg-3 col-md-6">
+          <div class="box">
+            <div class="img-box">
+              <img src="images/u-1.png" alt="">
+            </div>
+            <div class="detail-box">
+              <h5>
+                QUALITY EQUIPMENT
+              </h5>
+              <p>
+                ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+              </p>
             </div>
           </div>
-          <div class="col-lg-3 col-md-6">
-            <div class="box">
-              <div class="img-box">
-                <img src="images/u-4.png" alt="">
-              </div>
-              <div class="detail-box">
-                <h5>
-                  NUTRITION
-                </h5>
-                <p>
-                  ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                </p>
-              </div>
+        </div>
+        <div class="col-lg-3 col-md-6">
+          <div class="box">
+            <div class="img-box">
+              <img src="images/u-4.png" alt="">
+            </div>
+            <div class="detail-box">
+              <h5>
+                NUTRITION
+              </h5>
+              <p>
+                ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+              </p>
             </div>
           </div>
-          <div class="col-lg-3 col-md-6">
-            <div class="box">
-              <div class="img-box">
-                <img src="images/u-2.png" alt="">
-              </div>
-              <div class="detail-box">
-                <h5>
-                  HEALTHY DIET PLAN
-                </h5>
-                <p>
-                  ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                </p>
-              </div>
+        </div>
+        <div class="col-lg-3 col-md-6">
+          <div class="box">
+            <div class="img-box">
+              <img src="images/u-2.png" alt="">
+            </div>
+            <div class="detail-box">
+              <h5>
+                HEALTHY DIET PLAN
+              </h5>
+              <p>
+                ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+              </p>
             </div>
           </div>
-          <div class="col-lg-3 col-md-6">
-            <div class="box">
-              <div class="img-box">
-                <img src="images/u-3.png" alt="">
-              </div>
-              <div class="detail-box">
-                <h5>
-                  SPORT TRAINING
-                </h5>
-                <p>
-                  ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                </p>
-              </div>
+        </div>
+        <div class="col-lg-3 col-md-6">
+          <div class="box">
+            <div class="img-box">
+              <img src="images/u-3.png" alt="">
+            </div>
+            <div class="detail-box">
+              <h5>
+                SPORT TRAINING
+              </h5>
+              <p>
+                ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+              </p>
             </div>
           </div>
         </div>
       </div>
     </div>
-  </section>
+  </div>
+</section>
 
-  <!-- end us section -->
+<!-- end us section -->
 
 
-  <!-- heathy section -->
-
-  <section class="heathy_section layout_padding">
-    <div class="container">
-
-      <div class="row">
-        <div class="col-md-12 mx-auto">
-          <div class="detail-box">
-            <h2>
-              HEALTHY MIND, HEALTHY BODY
-            </h2>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillumLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-            </p>
-            <div class="btn-box">
-              <a href="">
-                READ MORE
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-
+<!-- heathy section -->
+<section class="heathy_section layout_padding">
+  <div class="container">
+    <div class="heading_container program-heading">
+      <h2>BUILDING PROGRAMS</h2>
     </div>
-  </section>
+    <?php list_all_programs(2); ?>
+  </div>
+</section>
 
-  <!-- end heathy section -->
+<!-- end heathy section -->
 
-  <!-- trainer section -->
+<!-- trainer section -->
 
-  <section class="trainer_section layout_padding">
-    <div class="container">
-      <div class="heading_container">
-        <h2>
-          <a href="<?php echo site_url('/our-trainers') ?>">
-            Our Gym Trainers
-          </a>
-        </h2>
-      </div>
-      <div class="row">
-        <?php list_all_trainers(3); ?>
-      </div>
+<section class="trainer_section layout_padding">
+  <div class="container">
+    <div class="heading_container">
+      <h2>
+        <a id="trainer-heading" href="<?php echo site_url('/our-trainers') ?>">
+          Our Gym Trainers
+        </a>
+      </h2>
     </div>
-  </section>
+    <div class="row">
+      <?php list_all_trainers(3); ?>
+    </div>
+  </div>
+</section>
 
-  <!-- end trainer section -->
+<!-- end trainer section -->
 
-  <!-- contact section -->
+<!-- contact section -->
 
 
-  <!-- end contact section -->
+<!-- end contact section -->
 
- 
+
 <?php get_footer(); ?>
