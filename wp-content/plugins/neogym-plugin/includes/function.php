30d04bec6387ca0813ecd3680746d49e747f22aa
kosta.norge@gmail.com <kosta.norge@gmail.com>
2024-02-03 21:09:25 +0100

    create functionality in func  program_joined, only if user is login and is not joined to program can join.

--- wp-content/plugins/neogym-plugin/includes/function.php
+++ wp-content/plugins/neogym-plugin/includes/function.php
@@ -123,20 +123,33 @@ function program_joined()
     }
 
     $post_id = esc_attr( $_POST [ 'post_id' ] );
-    $user_id = get_current_user_id(); 
+    $user_id = get_current_user_id();
    
     $joined_number = get_post_meta( $post_id, 'joined', true );
+    $joined_users = get_post_meta( $post_id, 'joined_users', true );
     
     if ( empty ( $joined_number ) ) {
         $joined_number = 0;
     }
 
-    update_post_meta( $post_id, 'joined', $joined_number + 1 );
-    wp_send_json( $joined_number + 1 );
+    if ( empty ( $joined_users ) ) {
+        $joined_users = array();
+    }
+    
+    if ( is_user_logged_in() && !in_array( $user_id, $joined_users) ) {
+        $joined_users[] = $user_id;
+
+        update_post_meta( $post_id, 'joined', $joined_number + 1 );
+        update_post_meta( $post_id, 'joined_users', $joined_users );
+        wp_send_json( $joined_number + 1 );
+    } else {
+        wp_send_json($joined_number);
+    }
 }
 
 add_action( 'wp_ajax_nopriv_program_joined', 'program_joined' );
 add_action( 'wp_ajax_program_joined', 'program_joined' );
+
 
 /**
  * Adds a top-level menu page.
