1d7a265e48d1fc76db966c3740f41d93a1346a4c
kosta.norge@gmail.com <kosta.norge@gmail.com>
2024-02-05 03:59:05 +0100

    add dynamic carousel in home page

--- wp-content/themes/neogym/templates/homepage.php
+++ wp-content/themes/neogym/templates/homepage.php
@@ -4,7 +4,60 @@
 <section class=" slider_section position-relative">
   <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
     <div class="carousel-inner">
-      <div class="carousel-item active">
+
+      <section class="slider_section position-relative">
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+          <div class="carousel-inner">
+            <?php
+            $args = array(
+              'post_type'      => 'gym',
+              'post_status'    => 'publish',
+              'posts_per_page' => 5,
+            );
+
+            $query = new WP_Query($args);
+            $counter = 0;
+
+            while ($query->have_posts()) : $query->the_post();
+              $active_class = ($counter === 0) ? 'active' : '';
+            ?>
+              <div class="carousel-item <?php echo esc_attr($active_class); ?>">
+                <div class="container">
+                  <div class="col-lg-10 col-md-11 mx-auto">
+                    <div class="detail-box">
+                      <div>
+                        <h1><?php the_title(); ?></h1>
+                        <p><?php the_content(); ?></p>
+                        <div>
+                          <div class="carousel-img img-box">
+                            <!-- <a href="<?php the_permalink(); ?>"> -->
+                            <?php the_post_thumbnail('post-thumbnail'); ?>
+                            <!-- </a> -->
+                          </div>
+                          <a href="<?php the_permalink(); ?>">Contact Us</a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            <?php
+              $counter++;
+            endwhile;
+
+            // Restore global post data
+            wp_reset_postdata();
+            ?>
+          </div>
+          <ol class="carousel-indicators">
+            <?php for ($i = 0; $i < $counter; $i++) : ?>
+              <li data-target="#carouselExampleIndicators" data-slide-to="<?php echo esc_attr($i); ?>" class="<?php echo ($i === 0) ? 'active' : ''; ?>"></li>
+            <?php endfor; ?>
+          </ol>
+        </div>
+      </section>
+
+      <!-- <div class="carousel-item active">
         <div class="container">
           <div class="col-lg-10 col-md-11 mx-auto">
             <div class="detail-box">
@@ -147,80 +200,81 @@
       <li data-target="#carouselExampleIndicators" data-slide-to="3"></li>
       <li data-target="#carouselExampleIndicators" data-slide-to="4"></li>
     </ol>
-  </div>
-</section>
-<!-- end slider section -->
-</div>
-
-
-<!-- Us section -->
-
-<section class="us_section layout_padding">
-  <div class="container">
-    <div class="heading_container">
-      <h2>
-        Why Choose Us
-      </h2>
+    </div>
+</section> -->
+      <!-- end slider section -->
     </div>
 
-    <div class="us_container ">
-      <div class="row">
-      <?php if( class_exists( 'CustomPostTypeLister' ) ) : ?>
-        <?php
-          $products_homepage_post_per_page = get_option( 'products_homepage_post_per_page' );
-          if ( empty ( $products_homepage_post_per_page ) ) {
-            $products_homepage_post_per_page = 2;
-          }
-          $product_lister = new CustomPostTypeLister( 'product' );
-          $product_lister->list_posts($products_homepage_post_per_page);
-        ?>
-      <?php endif; ?>
+
+    <!-- Us section -->
+
+    <section class="us_section layout_padding">
+      <div class="container">
+        <div class="heading_container">
+          <h2>
+            Why Choose Us
+          </h2>
+        </div>
+
+        <div class="us_container ">
+          <div class="row">
+            <?php if (class_exists('CustomPostTypeLister')) : ?>
+              <?php
+              $products_homepage_post_per_page = get_option('products_homepage_post_per_page');
+              if (empty($products_homepage_post_per_page)) {
+                $products_homepage_post_per_page = 2;
+              }
+              $product_lister = new CustomPostTypeLister('product');
+              $product_lister->list_posts($products_homepage_post_per_page);
+              ?>
+            <?php endif; ?>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-</section>
+    </section>
 
-<!-- end us section -->
+    <!-- end us section -->
 
 
-<!-- heathy section -->
-<section class="heathy_section layout_padding">
-  <div class="container">
-    <div class="heading_container program-heading">
-      <h2>BUILDING PROGRAMS</h2>
-    </div>
-    <?php if( class_exists( 'CustomPostTypeLister' ) ) : ?>
-      <?php
-        $program_lister = new CustomPostTypeLister( 'program' );
-        $program_lister->list_posts(2);
-      ?>
-    <?php endif; ?>
-  </div>
-</section>
+    <!-- heathy section -->
+    <section class="heathy_section layout_padding">
+      <div class="container">
+        <div class="heading_container program-heading">
+          <h2>BUILDING PROGRAMS</h2>
+        </div>
+        <?php if (class_exists('CustomPostTypeLister')) : ?>
+          <?php
+          $program_lister = new CustomPostTypeLister('program');
+          $program_lister->list_posts(2);
+          ?>
+        <?php endif; ?>
+      </div>
+    </section>
 
-<!-- end heathy section -->
+    <!-- end heathy section -->
 
-<!-- trainer section -->
+    <!-- trainer section -->
 
-<section class="trainer_section layout_padding">
-  <div class="container">
-    <div class="heading_container">
-      <h2>
-        <a id="trainer-heading" href="<?php echo site_url('/our-trainers') ?>">
-          Our Gym Trainers
-        </a>
-      </h2>
-    </div>
-    <div class="row">
-      <?php if( class_exists( 'CustomPostTypeLister' ) ) : ?>
-        <?php
-          $trainer_lister = new CustomPostTypeLister( 'trainer' );
-          $trainer_lister->list_posts(3);
-        ?>
-      <?php endif; ?>
-    </div>
-  </div>
-</section>
+    <section class="trainer_section layout_padding">
+      <div class="container">
+        <div class="heading_container">
+          <h2>
+            <a id="trainer-heading" href="<?php echo site_url('/our-trainers') ?>">
+              Our Gym Trainers
+            </a>
+          </h2>
+        </div>
+        <div class="row">
+          <?php if (class_exists('CustomPostTypeLister')) : ?>
+            <?php
+            $trainer_lister = new CustomPostTypeLister('trainer');
+            $trainer_lister->list_posts(3);
+            ?>
+          <?php endif; ?>
+        </div>
+      </div>
+    </section>
 
-<!-- end trainer section -->
-<?php get_footer(); ?>
+    <?php wp_reset_postdata(); ?>
+    <!-- end trainer section -->
+    <?php get_footer(); ?>
