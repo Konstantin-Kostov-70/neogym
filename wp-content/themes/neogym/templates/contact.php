2b45a429ccf859de884c8b420d6c54b621ebd77e
kosta.norge@gmail.com <kosta.norge@gmail.com>
2024-02-05 03:58:07 +0100

    create wpform and add to contact us page with short code

--- wp-content/themes/neogym/templates/contact.php
+++ wp-content/themes/neogym/templates/contact.php
@@ -13,12 +13,13 @@
         </div>
         <div class="col-lg-5 col-md-6">
           <div class="form_container pr-0 pr-lg-5 mr-0 mr-lg-2">
-            <div class="heading_container">
+            <div class="heading_container contact">
               <h2>
                 Contact Us
               </h2>
             </div>
-            <form action="">
+            <?php echo do_shortcode('[wpforms id="280"]');?>
+            <!-- <form action="">
               <div>
                 <input type="text" placeholder="Name" />
               </div>
@@ -36,7 +37,7 @@
                   Send
                 </button>
               </div>
-            </form>
+            </form> -->
           </div>
         </div>
       </div>
