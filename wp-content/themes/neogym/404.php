54a217bc2a1d6b257769c57159aaa7a6745ea804
kosta.norge@gmail.com <kosta.norge@gmail.com>
2024-02-05 03:48:37 +0100

    Create 404 page

---
+++ wp-content/themes/neogym/404.php
@@ -0,0 +1,27 @@
+<?php get_header(); ?>
+
+
+    <section class="heathy_section layout_padding">
+      <div class="container">
+
+        <div class="single">
+          <div class="col-md-12 mx-auto">
+            <div class="detail-box">
+              <h1 class="error-heading">
+                404 
+              </h1>
+              <p class="error-text">
+                <!-- <span class="error-span-1">O</span>
+                <span class="error-span-2">p</span>
+                <span class="error-span-3">p</span>
+                <span class="error-span-4">s</span> -->
+                OPPS PAGE NOT FOUND
+              </p>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section>
+
+<?php get_footer(); ?>
